9fedb34c03bec243645c8115231395dc978ab4eb
AlexBorous <alexborousas@gmail.com>
2021-04-23 16:43:29 +0300

    create camera and live_camera

--- camera_detection/lib/main.dart
+++ camera_detection/lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:camera/camera.dart';
+import 'package:camera_detection/live_camera.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+late List<CameraDescription> cameras;
+String url = "default URl";
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  cameras = await availableCameras();
+
   runApp(MyApp());
 }
 
@@ -9,10 +17,89 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: CameraApp(),
+    );
+  }
+}
+
+class CameraApp extends StatefulWidget {
+  @override
+  _CameraAppState createState() => _CameraAppState();
+}
+
+class _CameraAppState extends State<CameraApp> {
+  late TextEditingController _textEditingController;
+  @override
+  void initState() {
+    super.initState();
+
+    _textEditingController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(url);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Object Detector App"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.info),
+            onPressed: aboutDialog,
+          ),
+        ],
       ),
-      home: Container(),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ButtonTheme(
+                minWidth: 160,
+                child: ElevatedButton(
+                  child: Text("Real Time Detection"),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LiveFeed(
+                          cameras,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  aboutDialog() {
+    showAboutDialog(
+      context: context,
+      applicationName: "Object Detector App",
+      applicationLegalese: "By Alex Borousas",
+      applicationVersion: "1.0",
+      children: <Widget>[
+        TextField(
+          controller: _textEditingController,
+          onSubmitted: (str) {
+            url = str;
+          },
+        ),
+      ],
     );
   }
 }
